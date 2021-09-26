3d5c2059fe3b0d6401432a084ba912a4b265c365
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-09-26 15:36:20 +0100

    ci: set up workflow for stylelint

---
+++ next-sanity/stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    "stylelint-config-prettier",
+    "stylelint-config-sass-guidelines"
+  ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {}
+}
