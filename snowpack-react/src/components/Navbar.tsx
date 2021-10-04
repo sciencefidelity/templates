9ece289f04911575438fcfa9cf8adb31dd55c52c
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-10-04 23:18:04 +0100

    feat: create basic integration with react router

--- snowpack-react/src/components/Navbar.tsx
+++ snowpack-react/src/components/Navbar.tsx
@@ -1,21 +1,47 @@
 import React, { FC } from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 const Navbar: FC = () => {
   return (
-    <nav>
-      <ul>
-        <a href="/">
-          <li>Home</li>
-        </a>
-        <a href="/counter">
-          <li>Counter</li>
-        </a>
-        <a href="/about">
-          <li>About</li>
-        </a>
-      </ul>
-    </nav>
+    <Router>
+      <nav>
+        <ul>
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/counter">
+            <li>Counter</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/counter">
+          <Counter />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   )
+}
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Counter() {
+  return <h2>Counter</h2>
+}
+
+function About() {
+  return <h2>About</h2>
 }
 
 export default Navbar
