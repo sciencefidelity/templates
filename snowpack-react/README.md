c15e6ca01d1ae61182eb419fc96dd0857f8fef52
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-10-05 21:44:44 +0100

    build: set-up production build and serve pipeline

--- snowpack-react/README.md
+++ snowpack-react/README.md
@@ -26,6 +26,12 @@ Builds a static copy of your site to the `build/` folder.
 **For the best production performance:** Add a build bundler plugin like [@snowpack/plugin-webpack](https://github.com/snowpackjs/snowpack/tree/main/plugins/plugin-webpack) or [snowpack-plugin-rollup-bundle](https://github.com/ParamagicDev/snowpack-plugin-rollup-bundle) to your `snowpack.config.json` config file.
 
 ```shell
+yarn serve
+```
+
+Serve to production build from the build folder.
+
+```shell
 yarn clean
 ```
 
--- snowpack-react/snowpack.config.mjs
+++ snowpack-react/snowpack.config.mjs
@@ -8,21 +8,24 @@ export default {
   alias: {
     "@": "./src",
     components: "./src/components",
-    styles: "./src/styles",
+    styles: "./src/styles"
   },
   plugins: [
     "@snowpack/plugin-dotenv",
     "@snowpack/plugin-react-refresh",
     "@snowpack/plugin-sass",
-    "@snowpack/plugin-typescript"
+    "@snowpack/plugin-typescript",
+    "@snowpack/plugin-webpack"
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
-    {"match": "routes", "src": ".*", "dest": "/index.html"},
+    { match: "routes", src: ".*", dest: "/index.html" }
   ],
   optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
+    /* enable the built-in esbuild bundler, remove webpack-plugin to use */
+    // bundle: true,
+    // minify: true,
+    // target: "es2018"
   },
   packageOptions: {
     /* ... */
