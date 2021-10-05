dfab46e34304269e2e15fd48da77a158039fd098
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-10-05 16:33:08 +0100

    test: set up initial test suites

--- snowpack-react/src/pages/__tests__/Counter.test.tsx
+++ snowpack-react/src/pages/__tests__/Counter.test.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
 import { render } from "@testing-library/react"
 import { expect } from "chai"
-import Home from "@/pages/Home"
+import Counter from "@/pages/Home"
 
-describe("<Home>", () => {
-  it("the header title", () => {
-    const { getByText } = render(<Home title="home" />)
+describe("<App>", () => {
+  it("renders learn react link", () => {
+    const { getByText } = render(<Counter title="counter" />)
     const linkElement = getByText(/Create-React-Snowpack-App/i)
     expect(document.body.contains(linkElement))
   })
