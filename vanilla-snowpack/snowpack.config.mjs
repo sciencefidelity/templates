ff414042b1fe0bcc248873dfc9cd2b9bdf8a8cd2
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-10-10 23:03:37 -0200

    feat: add vanilla snowpack template

---
+++ vanilla-snowpack/snowpack.config.mjs
@@ -0,0 +1,23 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' }
+  },
+  plugins: [
+    "@snowpack/plugin-sass",
+    "@snowpack/plugin-typescript"
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    open: "none"
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
---
+++ vanilla-snowpack/src/index.ts
@@ -0,0 +1,9 @@
+const counter = document.querySelector("#counter") as HTMLSpanElement
+let seconds = 0
+
+setInterval(() => {
+  seconds += 1
+  counter.textContent = seconds.toString()
+}, 1000)
+
+export {}
