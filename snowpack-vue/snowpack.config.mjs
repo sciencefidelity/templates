6fb3f1641a27fc19a6847ee030dd998d2ddb89ba
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-11-29 22:05:53 +0000

    feat: set up base files for template

--- snowpack-vue/snowpack.config.mjs
+++ snowpack-vue/snowpack.config.mjs
@@ -4,7 +4,7 @@ export default {
     public: "/",
     src: "/dist"
   },
-  plugins: ["@snowpack/plugin-vue"],
+  plugins: ["@snowpack/plugin-dotenv", "@snowpack/plugin-vue"],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
