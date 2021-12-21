be35aedabe2287810b9255ffaa2cf897c1d955b3
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-21 23:10:44 +0000

    refactor: remove template

--- next-sanity/lib/queries.ts
+++
@@ -1,9 +0,0 @@
-import { groq } from "next-sanity"
-
-export const postQuery = groq`
-  *[_type == "post"][0]{
-    _id, title, body, publishedAt,
-    "author": author->name,
-    categories[]->{_id, title}
-  }
-`
--- next-sanity/stylelint.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    "stylelint-config-prettier",
-    "stylelint-config-recommended",
-    "stylelint-config-sass-guidelines"
-  ],
-  // https://stylelint.io/user-guide/configuration
-  rules: {}
-}
