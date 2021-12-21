6d6f74d1237a24066a747e76d60acabc95a2b064
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-21 23:08:38 +0000

    feat: set up file structure and build first page

--- vite-vue/src/App.vue
+++ vite-vue/src/App.vue
@@ -1,21 +1,37 @@
-<script setup>
+<script lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { Vue, Options } from "vue-class-component"
+import Timer from "./components/Timer.vue"
+
+@Options({
+  name: "App",
+  components: {
+    Timer
+  }
+})
+export default class App extends Vue {}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Timer />
 </template>
 
-<style>
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  &::after,
+  &::before {
+    box-sizing: border-box;
+    margin: 0;
+  }
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
