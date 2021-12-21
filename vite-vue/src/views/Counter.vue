1a0fdb092df446203cce4637d346efd6c1a01df1
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-21 23:58:12 +0000

    feat: copy views from snowpack-vue

---
+++ vite-vue/src/views/Counter.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { Vue, Options } from "vue-class-component"
+@Options({})
+export default class App extends Vue {}
+</script>
+
+<template>
+  <Layout>
+    <main class="main">
+    </main>
+  </Layout>
+</template>
+
+<style lang="scss">
+.main {
+  background-color: #f9f6f6;
+  color: #333;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  &__logo {
+    height: 36vmin;
+    pointer-events: none;
+    margin-bottom: 3rem;
+    animation: logo-pulse infinite 1.6s ease-in-out alternate;
+  }
+}
+</style>
---
+++ vite-vue/src/views/Home.vue
@@ -0,0 +1,57 @@
+<script lang="ts">
+import { Vue, Options } from "vue-class-component"
+import Layout from "../layouts/Layout.vue"
+@Options({
+  components: {
+    Layout
+  }
+})
+export default class App extends Vue {
+  count = 0
+  mounted() {
+    const interval = setInterval(() => this.count++, 1000)
+    return () => {
+      clearInterval(interval)
+    }
+  }
+}
+</script>
+
+<template>
+  <Layout>
+    <main class="main">
+      <img src="/logo.svg" class="main__logo" alt="logo" />
+      <p>
+        Page has been open for <code>{{ count }}</code> seconds.
+      </p>
+    </main>
+  </Layout>
+</template>
+
+<style lang="scss">
+@keyframes logo-pulse {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.06);
+  }
+}
+
+.main {
+  background-color: #f9f6f6;
+  color: #333;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  &__logo {
+    height: 36vmin;
+    pointer-events: none;
+    margin-bottom: 3rem;
+    animation: logo-pulse infinite 1.6s ease-in-out alternate;
+  }
+}
+</style>
