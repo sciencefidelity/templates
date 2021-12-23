eeb8b17294e882941d2f2c627719d85d7a60ffd3
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-23 12:42:32 +0000

    feat: bootstrap new svelte template with showpack

---
+++ snowpack-svelte/src/index.js
@@ -0,0 +1,17 @@
+/* Add JavaScript code here! */
+import App from "./App.svelte"
+
+let app = new App({
+  target: document.body,
+})
+
+export default app
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+if (undefined /* [snowpack] import.meta.hot */ ) {
+  undefined /* [snowpack] import.meta.hot */ .accept()
+  undefined /* [snowpack] import.meta.hot */ .dispose(() => {
+    app.$destroy()
+  })
+}
