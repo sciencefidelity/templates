3549dbdec5f953123eec49ae67a054806d9ca94d
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-25 11:41:18 +0000

    feat: copy src from vite-svelte

--- snowpack-svelte/src/main.ts
+++ snowpack-svelte/src/main.ts
@@ -3,10 +3,10 @@ import "sanitize.css/sanitize.css"
 import "sanitize.css/assets.css"
 import "sanitize.css/reduce-motion.css"
 import "sanitize.css/typography.css"
-import "./styles/styles.scss"
+import "@/styles/styles.scss"
 
 const app = new App({
-  target: document.getElementById("app")
+  target: document.getElementById("app") as HTMLElement
 })
 
 export default app
