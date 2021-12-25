352b00a013bf7f5cb46d9dd1de60f10bbaf865bc
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-25 08:17:23 +0000

    fix: get scss working in components

--- vite-svelte/src/lib/Navbar.svelte
+++ vite-svelte/src/lib/Navbar.svelte
@@ -1,32 +1,29 @@
 <style lang="scss">
   nav {
     background-color: var(--red);
+    color: var(--grey-000);
     display: grid;
     font-weight: 600;
     padding: 1rem 2rem;
     place-items: center;
     text-align: center;
     width: 100%;
-  }
-
-  nav a {
-    color: var(--grey-000);
-    text-decoration: none;
-  }
-
-  nav a:hover {
-    text-decoration: underline;
-  }
-
-  ul {
-    display: flex;
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-
-  ul li {
-    padding: 0 2rem;
+    a {
+      color: var(--grey-000);
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+    ul {
+      display: flex;
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      li {
+        padding: 0 2rem;
+      }
+    }
   }
 </style>
 
--- vite-svelte/vite.config.ts
+++ vite-svelte/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite"
 import { resolve } from "path"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
+import preprocess from "svelte-preprocess"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte({ preprocess: preprocess() })],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src")
