fa36df9c2aaafb1e5b1d011985afe5dcc4ccb341
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-25 11:12:20 +0000

    fix: add type to html element

--- vite-svelte/src/main.ts
+++ vite-svelte/src/main.ts
@@ -6,7 +6,7 @@ import "sanitize.css/typography.css"
 import "@/styles/styles.scss"
 
 const app = new App({
-  target: document.getElementById("app")
+  target: document.getElementById("app") as HTMLElement
 })
 
 export default app
