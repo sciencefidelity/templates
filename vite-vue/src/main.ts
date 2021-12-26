cd13315cb97f2dcf6043f64a0a0a94e3cafa5a2d
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-26 12:10:33 +0000

    chore: remove unused files and prettify

--- vite-vue/src/main.ts
+++ vite-vue/src/main.ts
@@ -1,14 +1,12 @@
 import { createApp } from "vue"
 import App from "@/App.vue"
-import router from "@/router/index"
+import router from "./router/index"
 import "sanitize.css/sanitize.css"
 import "sanitize.css/assets.css"
 import "sanitize.css/reduce-motion.css"
 import "sanitize.css/typography.css"
 import "@/styles/globals.scss"
-import Layout from "@/layouts/Layout.vue"
 
 createApp(App)
   .use(router)
-  .component("Layout", Layout)
   .mount("#app")
