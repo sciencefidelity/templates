d134def6db2a40ab46b4e1a145a80e7ab915cc01
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-28 19:12:32 +0000

    test: set up paths and correct path in tests

--- vite-vanilla/src/main.ts
+++ vite-vanilla/src/main.ts
@@ -2,7 +2,7 @@ import "sanitize.css/sanitize.css"
 import "sanitize.css/assets.css"
 import "sanitize.css/reduce-motion.css"
 import "sanitize.css/typography.css"
-import "./style.scss"
+import "@/style.scss"
 
 const counter = document.querySelector("#counter") as HTMLSpanElement
 let seconds = 0
