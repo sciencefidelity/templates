09e44ba6eedf710fb9f5ab219b8625e224ae5230
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-29 23:44:43 +0000

    build: set up stylelint and fix styles

--- vite-react/jest.config.ts
+++ vite-react/jest.config.ts
@@ -5,18 +5,17 @@ const paths = pathsToModuleNameMapper(compilerOptions.paths, {
   prefix: "<rootDir>/"
 })
 
-const config: Config.InitialOptions =  {
+const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   transform: {
     "node_modules/react/.+\\.(j|t)sx?$": "ts-jest"
   },
-  transformIgnorePatterns: [
-    "node_modules/(?!react/.*)"
-  ],
+  transformIgnorePatterns: ["node_modules/(?!react/.*)"],
   moduleNameMapper: {
     ".+\\.(css|sass|scss)$": "identity-obj-proxy",
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.ts",
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.ts",
     ...paths
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"]
