6db940958d8c388290be03d7655de306fc7c39de
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-31 08:29:28 +0000

    build: add configs for prettier, eslint and stylelint

--- svelte-kit/.eslintrc.cjs
+++ svelte-kit/.eslintrc.cjs
@@ -1,24 +1,32 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier"
   ],
   plugins: ["svelte3", "@typescript-eslint"],
-  ignorePatterns: ["*.cjs"],
-  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
-  settings: {
-    "svelte3/typescript": () => require("typescript")
-  },
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2020
   },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true
+  ignorePatterns: ["*.cjs"],
+  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "never"]
+  },
+  settings: {
+    "svelte3/typescript": () => require("typescript"),
+    "svelte3/ignore-styles": attributes =>
+      attributes.lang && attributes.lang.includes("scss")
   }
 }
