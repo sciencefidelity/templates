7886df08e6d6b8748fa84e50177a2feeb44bc23a
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-04-11 23:15:59 +0100

    refactor: format

--- vite-lit/vite.config.ts
+++ vite-lit/vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite"
 import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/my-element.ts',
-      formats: ['es']
+      entry: "src/my-element.ts",
+      formats: ["es"]
     },
     rollupOptions: {
       external: /^lit/
