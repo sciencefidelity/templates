5bc34d2162ac8a7d6b5d4b21f03d913ec08e5f85
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-18 23:45:04 +0100

    chore: remove templates that are no longer useful

--- snowpack-react/web-test-runner.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env node */
-process.env.NODE_ENV = "test"
-
-module.exports = {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  plugins: [require("@snowpack/web-test-runner-plugin")()]
-}
