0edf810239e2836508fc010b05c6b64bd33ed78c
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-05 21:47:14 -0100

    refactor: change file watcher to ts-node-dev

--- ts-jest/index.test.ts
+++ ts-jest/index.test.ts
@@ -1,5 +1,45 @@
-import { sum } from "./index"
+import { fib } from "./index"
 
-test("adds 1 + 2 to equal 3", () => {
-	expect(sum(1, 2)).toBe(3)
+test("Fibonacci number 0", () => {
+  expect(fib(0)).toEqual(0)
+})
+
+test("Fibonacci number 1", () => {
+  expect(fib(1)).toEqual(1)
+})
+
+test("Fibonacci number 2", () => {
+  expect(fib(2)).toEqual(1)
+})
+
+test("Fibonacci number 3", () => {
+  expect(fib(3)).toEqual(2)
+})
+
+test("Fibonacci number 4", () => {
+  expect(fib(4)).toEqual(3)
+})
+
+test("Fibonacci number 5", () => {
+  expect(fib(5)).toEqual(5)
+})
+
+test("Fibonacci number 6", () => {
+  expect(fib(6)).toEqual(8)
+})
+
+test("Fibonacci number 7", () => {
+  expect(fib(7)).toEqual(13)
+})
+
+test("Fibonacci number 8", () => {
+  expect(fib(8)).toEqual(21)
+})
+
+test("Fibonacci number 9", () => {
+  expect(fib(9)).toEqual(34)
+})
+
+test("Fibonacci number 10", () => {
+  expect(fib(10)).toEqual(55)
 })
--- ts-jest/index.ts
+++ ts-jest/index.ts
@@ -1,3 +1,15 @@
-export const sum = (a: number, b: number) => {
-	return a + b
+const memo: number[] = []
+
+export function fib(n: number): number {
+  return fibMem(n, memo)
 }
+
+function fibMem(n: number, memo: number[]): number {
+  if (n === 0) return 0
+  if (n < 2) return 1
+  if (memo[n] > 0) return memo[n]
+  memo[n] = fibMem(n - 1, memo) + fibMem(n - 2, memo)
+  return memo[n]
+}
+
+console.log(fib(10), 55)
