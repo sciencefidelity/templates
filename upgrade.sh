724a2de3a72b37b02bf6285d89689dba92905498
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-05 22:24:41 -0100

    chore: upgrade all deps

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 # run `yarn remove` and `yarn add` for every dependency in package.json
 npx yarn-upgrade-all &&
 for d in */; do cd "$d"; npx yarn-upgrade-all; cd ..; done
