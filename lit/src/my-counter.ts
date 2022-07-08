cf6820ac1db923bec3e1b8cdb27a98270f4e97a3
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-08 22:06:43 +0000

    refactor: disable button when count is zero

--- lit/src/my-counter.ts
+++ lit/src/my-counter.ts
@@ -49,26 +49,47 @@ export class MyCounter extends LitElement {
   @property({ type: Number })
   count = 0
 
+  /**
+   * Disable decrement button when count is zero.
+   */
+  @property({ type: Number })
+  disabled = true
+
   render() {
     return html`
       <div class="counter">
         <h1>${this.name}</h1>
-        <button @click=${this._decrement} part="button">-</button>
+        <button
+          @click=${this._decrement}
+          part="button"
+          ?disabled=${this.disabled}
+        >
+          -
+        </button>
         <div class="count-buttons">${this.count}</div>
         <button @click=${this._increment} part="button">+</button>
         <slot />
-      <div>
+        <div></div>
+      </div>
     `
   }
 
   private _decrement() {
+    const display = document.querySelector(".count-display") as HTMLElement
     this.count--
-    document.querySelector(".count-display")!.innerText = this.count
+    if (this.count <= 0) {
+      this.disabled = true
+    }
+    display!.innerText = this.count.toString()
   }
 
   private _increment() {
+    const display = document.querySelector(".count-display") as HTMLElement
     this.count++
-    document.querySelector(".count-display")!.innerText = this.count
+    if (this.count > 0) {
+      this.disabled = false
+    }
+    display!.innerText = this.count.toString()
   }
 }
 
