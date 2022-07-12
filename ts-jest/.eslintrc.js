42d5de70619241f548cdd0e53ec6edfaeec23d7e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-12 19:07:45 +0100

    build: remove editorconfig in packages

---
+++ ts-jest/.eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  plugins: [
+    "@typescript-eslint",
+    "eslint-comments",
+    "jest",
+    "promise",
+    "unicorn",
+  ],
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:eslint-comments/recommended",
+    "plugin:jest/recommended",
+    "plugin:promise/recommended",
+    "plugin:unicorn/recommended",
+    "prettier",
+  ],
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./ts-jest/tsconfig.json",
+  },
+  rules: {
+    // console is preferred in a Node project
+    "no-console": "off",
+    // https://basarat.gitbook.io/typescript/main-1/defaultisbad
+    "import/prefer-default-export": "off",
+    "import/no-default-export": "error",
+    // It's not accurate in the monorepo style
+    "import/no-extraneous-dependencies": "off",
+    // Allow most functions to rely on type inference
+    // @typescript-eslint/explicit-module-boundary-types
+    // will ensure exports are typed
+    "@typescript-eslint/explicit-function-return-type": "off",
+    // Airbnb prefers forEach
+    "unicorn/no-array-for-each": "off",
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        // Allow CJS until ESM support improves
+        "@typescript-eslint/no-var-requires": "off",
+        "unicorn/prefer-module": "off",
+      },
+    },
+  ],
+};
