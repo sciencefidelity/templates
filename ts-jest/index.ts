be9570de3fd2504ef7d8a1247c6d9f463631f149
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-12 19:18:13 +0100

    refactor: improve function thanks to eslint

--- ts-jest/index.ts
+++ ts-jest/index.ts
@@ -1,15 +1,15 @@
 const memo: number[] = [];
 
-function fibMem(n: number, memo: number[]): number {
+function fibMem(n: number) {
   if (n === 0) return 0;
   if (n < 2) return 1;
   if (memo[n] > 0) return memo[n];
-  memo[n] = fibMem(n - 1, memo) + fibMem(n - 2, memo);
+  memo[n] = fibMem(n - 1) + fibMem(n - 2);
   return memo[n];
 }
 
-export function fib(n: number): number {
-  return fibMem(n, memo);
+export function fib(n: number) {
+  return fibMem(n);
 }
 
-console.log(fib(10), 55);
+console.log(fib(8), 55);
