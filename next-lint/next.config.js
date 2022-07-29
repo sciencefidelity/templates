a759c07839b2db4cd5255028b4204a5b89422fcf
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-29 23:57:26 +0100

    refactor: rename directory

---
+++ next-lint/next.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line tsdoc/syntax
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [],
+  },
+};
+
+module.exports = nextConfig;
