21b86a1c60228f8e2d2da538231a8ad9a694bb5b
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-30 23:24:52 +0100

    refactor: use double quotes

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,6 +1,6 @@
-import 'modern-normalize'
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import "modern-normalize"
+import "../styles/globals.scss"
+import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
