24c06f954bf56ea13310fd4f149353ea3f3c9913
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-31 15:44:44 +0100

    refactor: create new react template with stricter lint rules

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import { App } from "./app"
+import { App } from "./App"
 import "modern-normalize"
 import "styles/globals.scss"
 
