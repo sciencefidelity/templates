c9360dbedc0d743aa48181359b0014136104fe36
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-08-11 15:29:02 +0100

    feat: change stories to svelte components

---
+++ svelte-storybook/ui-vite/src/stories/button/Button.stories.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+
+	const argTypes = {
+		backgroundColor: { control: 'color' },
+		label: { control: 'text' },
+		onClick: { action: 'onClick' },
+		primary: { control: 'boolean' },
+		size: {
+			control: { type: 'select' },
+			options: ['small', 'medium', 'large']
+		}
+	};
+</script>
+
+<Meta title="Example/Button" component={Button} {argTypes} />
+
+<Template let:args>
+	<Button component={Button} {...args} on:click={args.onClick} />
+</Template>
+
+<Story name="Primary" args={{ primary: true, label: 'Button' }} />
+
+<Story name="Secondary" args={{ label: 'Button' }} />
+
+<Story name="Large" args={{ size: 'large', label: 'Button' }} />
+
+<Story name="Small" args={{ size: 'small', label: 'Button' }} />
