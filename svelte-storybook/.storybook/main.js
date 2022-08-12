07d3ad3d16bd372156e91487374546e1935550cf
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-08-12 22:43:31 +0100

    build: change storybook config back to js

---
+++ svelte-storybook/.storybook/main.js
@@ -0,0 +1,23 @@
+const preprocess = require('svelte-preprocess')
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-svelte-csf',
+    'storybook-addon-mock/register',
+  ],
+  framework: '@storybook/svelte',
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  svelteOptions: {
+    preprocess: preprocess({
+      typescript: true,
+      postcss: true,
+      sourceMap: true,
+    }),
+  },
+}
